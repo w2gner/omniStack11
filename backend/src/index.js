83f89af5857050dbcad3bca3d670df4fc4c18999
Wagner <wagnerghedin10@gmail.com>
2020-03-24 20:49:45 -0300

    Application backend

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+app.use(cors()); // determinar a origin: '' caso o ambiente seja produção
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
