5aa98754722fe3114cad9c2ee30e82326ea06982
Wagner <wagnerghedin10@gmail.com>
2020-03-28 14:19:49 -0300

    Created validations for endpoints and tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 const app = express();
 
 app.use(cors()); // determinar a origin: '' caso o ambiente seja produção
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.use(errors());
+
+module.exports = app;
